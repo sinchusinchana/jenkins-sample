7b3abfc691a8767994786dbb51a8559d8da723ae
sinchana <sinchana@gmail.com>
2021-12-07 13:00:11 +0530

    first commit

---
+++ src/test/java/com/sinchu/jenkins_sample/AppTest.java
@@ -0,0 +1,20 @@
+package com.sinchu.jenkins_sample;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
